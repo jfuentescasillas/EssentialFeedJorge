5e461cef72f26496de19a6b3962e2efacde2ac55
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-02 10:24:59 +0200

    Create FeedItem Model

---
+++ EssentialFeedJorge/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  EssentialFeedJorge
+//
+//  Created by jfuentescasillas on 02/08/23.
+//
+
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
