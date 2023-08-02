963e8d7351420097e45efa807e74504deb37ffb3
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-02 14:18:26 +0200

    Give URL instead of hardcode it in the RemoteFeedLoader

--- EssentialFeedJorgeTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedJorgeTests/RemoteFeedLoaderTests.swift
@@ -11,15 +11,17 @@ import XCTest
 
 // MARK: - RemoteFeedLoader
 class RemoteFeedLoader {
+    let url: URL
     let client: HTTPClient
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com"))
+        client.get(from: url)
     }
 }
 
@@ -42,8 +44,9 @@ class HTTPClientSpy: HTTPClient {
 // MARK: - Tests Class
 final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        let _ = RemoteFeedLoader(client: client)  // SUT not used in this case
+        let _ = RemoteFeedLoader(url: url, client: client)  // SUT not used in this case
         
         XCTAssertNil(client.requestedURL)
     }
@@ -51,13 +54,28 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_requestDataFromURL() {
         // ARRANGE: Given a SUT (System Under Test) and a client
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         // ACT: When we invoke sut.load()
         sut.load()
         
         // ASSERT: Then assert that a URL request was initiated in the client
         XCTAssertNotNil(client.requestedURL)
+    }
+    
+    
+    func test_load_requestDataFromURLAndURLsAreEqual() {
+        // ARRANGE: Given a SUT (System Under Test) and a client
+        let url = URL(string: "https://a-given-url.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        // ACT: When we invoke sut.load()
+        sut.load()
+        
+        // ASSERT: Then assert that a URL request was initiated in the client
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
