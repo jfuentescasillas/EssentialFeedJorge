986cf6e00680cb9f4ca2939a32f91bb6d6c8c6c7
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-02 10:22:47 +0200

    Initial Commit

---
+++ EssentialFeedJorge/EssentialFeedJorge.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedJorge.h
+//  EssentialFeedJorge
+//
+//  Created by jfuentescasillas on 02/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedJorge.
+FOUNDATION_EXPORT double EssentialFeedJorgeVersionNumber;
+
+//! Project version string for EssentialFeedJorge.
+FOUNDATION_EXPORT const unsigned char EssentialFeedJorgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedJorge/PublicHeader.h>
+
+
