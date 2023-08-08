083028286e315c9f821c5f959da8d26d463a0ef4
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-08 13:10:33 +0200

    Move all the mapping logic to the FeedItemsMapper

--- EssentialFeedJorge/FeedAPI/FeedItemsMapper.swift
+++ EssentialFeedJorge/FeedAPI/FeedItemsMapper.swift
@@ -12,9 +12,13 @@ import Foundation
 internal final class FeedItemsMapper {
     private struct Root: Decodable {
         let items: [Item]
+        
+        var feed: [FeedItem] {
+            return items.map { $0.item }
+        }
     }
-
-
+    
+    
     private struct Item: Decodable {
         let id: UUID
         let description: String?
@@ -36,13 +40,14 @@ internal final class FeedItemsMapper {
     private static var OK_200: Int { return 200 }
     
     
-    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
+    // MARK: - Private Methods
+    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard response.statusCode == OK_200,
+              let root = try? JSONDecoder().decode(Root.self, from: data)
+        else {
+            return .failure(.invalidData)
         }
         
-        let mappedItems = try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
-        
-        return mappedItems
+        return .success(root.feed)
     }
 }
--- EssentialFeedJorge/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeedJorge/FeedAPI/RemoteFeedLoader.swift
@@ -42,23 +42,11 @@ public final class RemoteFeedLoader {
             
             switch result {
             case let .success(data, response):
-                completion(self.map(data, from: response))
+                completion(FeedItemsMapper.map(data, from: response))
 
             case .failure:
                 completion(.failure(.connectivity))
             }
         }
-    }
-    
-    
-    // MARK: - Private Methods
-    private func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let items = try FeedItemsMapper.map(data, response)
-           
-            return .success(items)
-        } catch {
-            return .failure(.invalidData)
-        }
-    }
+    }    
 }
