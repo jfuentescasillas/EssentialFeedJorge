c616f78ec21c96893e5a3c937f6954df22ad3816
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-09 13:30:30 +0200

    Resumes dataTask on creation

--- EssentialFeedJorgeTests/URLSessionHTTPClientTests.swift
+++ EssentialFeedJorgeTests/URLSessionHTTPClientTests.swift
@@ -12,13 +12,14 @@ import XCTest
 class URLSessionHTTPClient {
     private let session: URLSession!
     
+    
     init(session: URLSession!) {
         self.session = session
     }
     
     
     func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }
+        session.dataTask(with: url) { _, _, _ in }.resume()
     }
 }
 
@@ -31,13 +32,28 @@ class URLSessionHTTPClientTests: XCTestCase {
         sut.get(from: url)
         
         XCTAssertEqual(session.receivedURLs, [url])
-}
+    }
+    
+    
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "https://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        
+        session.stub(url: url, task: task)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
     
     
     // MARK: - Helpers
     private class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
-
+        private var stubs = [URL: URLSessionDataTask]()
+        
         
         override init() {
             super.init()
@@ -48,7 +64,12 @@ class URLSessionHTTPClientTests: XCTestCase {
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(url)
             
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
+        }
+        
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
         }
     }
     
@@ -56,6 +77,24 @@ class URLSessionHTTPClientTests: XCTestCase {
     private class FakeURLSessionDataTask: URLSessionDataTask {
         override init() {
             // Initialize any properties or perform any setup needed for FakeURLSessionDataTask
+        }
+        
+        
+        override func resume() {}
+    }
+    
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount: Int = 0
+        
+        
+        override init() {
+            // Initialize any properties or perform any setup needed for FakeURLSessionDataTask
+        }
+        
+        
+        override func resume() {
+            resumeCallCount += 1
         }
     }
 }
