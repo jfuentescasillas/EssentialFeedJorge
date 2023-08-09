533d5d8c1f24285e7db8ad38ea2219c76070471c
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-09 13:54:35 +0200

    Refactor URLSession subclass mocking with protocol based mocking

--- EssentialFeedJorgeTests/URLSessionHTTPClientTests.swift
+++ EssentialFeedJorgeTests/URLSessionHTTPClientTests.swift
@@ -10,11 +10,21 @@ import XCTest
 import EssentialFeedJorge
 
 
+protocol HTTPSessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTaskProtocol
+}
+
+
+protocol HTTPSessionTaskProtocol {
+    func resume()
+}
+
+
 class URLSessionHTTPClient {
-    private let session: URLSession!
+    private let session: HTTPSessionProtocol
     
     
-    init(session: URLSession!) {
+    init(session: HTTPSessionProtocol) {
         self.session = session
     }
     
@@ -32,7 +42,7 @@ class URLSessionHTTPClient {
 class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "https://any-url.com")!
-        let session = URLSessionSpy()
+        let session = HTTPSessionSpy()
         let task = URLSessionDataTaskSpy()
         
         session.stub(url: url, task: task)
@@ -47,7 +57,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError() {
         let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
-        let session = URLSessionSpy()
+        let session = HTTPSessionSpy()
         session.stub(url: url, error: error)
         
         let exp = expectation(description: "Waiting for completion")
@@ -56,7 +66,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError, error)
-            
+                
             default:
                 XCTFail("Expected failure with error \(error) but got \(result) instead.")
             }
@@ -69,9 +79,9 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     
     // MARK: - Helpers
-    private class URLSessionSpy: URLSession {
+    private class HTTPSessionSpy: HTTPSessionProtocol {
         private struct Stub {
-            let task: URLSessionDataTask
+            let task: HTTPSessionTaskProtocol
             let error: Error?
         }
         
@@ -79,13 +89,12 @@ class URLSessionHTTPClientTests: XCTestCase {
         private var stubs = [URL: Stub]()
         
         
-        override init() {
-            super.init()
-            // Your initialization code for URLSessionSpy here
+        func stub(url: URL, task: HTTPSessionTaskProtocol = FakeURLSessionDataTask(), error: Error? = nil) {
+            stubs[url] = Stub(task: task, error: error)
         }
         
         
-        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTaskProtocol {
             guard let stub = stubs[url] else {
                 fatalError("Couldn't find stub for \(url)")
             }
@@ -94,34 +103,19 @@ class URLSessionHTTPClientTests: XCTestCase {
             
             return stub.task
         }
-        
-        
-        func stub(url: URL, task: URLSessionDataTask = FakeURLSessionDataTask(), error: Error? = nil) {
-            stubs[url] = Stub(task: task, error: error)
-        }
     }
     
     
-    private class FakeURLSessionDataTask: URLSessionDataTask {
-        override init() {
-            // Initialize any properties or perform any setup needed for FakeURLSessionDataTask
-        }
-        
-        
-        override func resume() {}
+    private class FakeURLSessionDataTask: HTTPSessionTaskProtocol {
+        func resume() {}
     }
     
     
-    private class URLSessionDataTaskSpy: URLSessionDataTask {
+    private class URLSessionDataTaskSpy: HTTPSessionTaskProtocol {
         var resumeCallCount: Int = 0
         
         
-        override init() {
-            // Initialize any properties or perform any setup needed for FakeURLSessionDataTask
-        }
-        
-        
-        override func resume() {
+        func resume() {
             resumeCallCount += 1
         }
     }
