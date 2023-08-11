d7d1940819deab81c704d602795e870146968cf6
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-11 17:19:36 +0200

    Update Timeout to 10 seconds

--- EssentialFeedJorgeAPIEndToEndTests/EssentialFeedJorgeAPIEndToEndTests.swift
+++ EssentialFeedJorgeAPIEndToEndTests/EssentialFeedJorgeAPIEndToEndTests.swift
@@ -57,7 +57,7 @@ final class EssentialFeedJorgeAPIEndToEndTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 35)
+        wait(for: [exp], timeout: 10)
         
         return receivedResult
     }
