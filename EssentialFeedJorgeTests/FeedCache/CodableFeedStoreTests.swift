b0c431a1e15afd6b8c7d8d6e7e1157838e989951
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-24 13:11:19 +0200

    Retrieving from invalid cache twice delivers same failure result (no-side-effects)

--- EssentialFeedJorgeTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedJorgeTests/FeedCache/CodableFeedStoreTests.swift
@@ -146,6 +146,16 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+        let storeURL = testSpecificStoreURL ()
+        let sut = makeSUT(storeURL: storeURL)
+        
+        try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
+        
+        expect(sut, toRetrieveTwice: .failure(anyNSError()))
+    }
+    
+    
     // MARK: - Helper Methods
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
         let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
