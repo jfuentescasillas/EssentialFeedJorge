2c839089939ae64ce016c5aa77c06d1eaa1682e7
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-29 13:17:35 +0200

    Incluye cache test helpers in the cache integration tests target

--- EssentialFeedJorgeCacheIntegrationTests/EssentialFeedJorgeCacheIntegrationTests.swift
+++ EssentialFeedJorgeCacheIntegrationTests/EssentialFeedJorgeCacheIntegrationTests.swift
@@ -31,6 +31,36 @@ final class EssentialFeedJorgeCacheIntegrationTests: XCTestCase {
     }
     
     
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueImageFeed().models
+        
+        let saveExp = expectation(description: "Wait for save completion")
+        sutToPerformSave.save(feed: feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp.fulfill()
+        }
+        
+        wait(for: [saveExp], timeout: 1)
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, feed)
+                
+            case let .failure(error):
+                XCTFail("Expected successful feed result,got \(error) instead")
+            }
+            
+            loadExp.fulfill()
+        }
+        
+        wait(for: [loadExp], timeout: 1)
+    }
+    
+    
     // MARK: - Helper Methods
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
