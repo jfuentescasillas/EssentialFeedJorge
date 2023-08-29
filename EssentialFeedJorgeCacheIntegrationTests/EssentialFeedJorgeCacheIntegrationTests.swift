5df75235acfe132b6cb4a16e365598305e5f6440
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-08-29 10:19:13 +0200

    Add "EssentialFeedJorgeCacheIntegrationTests" target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedJorgeCacheIntegrationTests/EssentialFeedJorgeCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedJorgeCacheIntegrationTests.swift
+//  EssentialFeedJorgeCacheIntegrationTests
+//
+//  Created by jfuentescasillas on 29/08/23.
+//
+
+import XCTest
+
+final class EssentialFeedJorgeCacheIntegrationTests: XCTestCase {
+    
+}
