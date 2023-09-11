0201e30b4e2a966e08dc08890a4e1875e20ec234
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-09-11 12:46:33 +0200

    Add Prototype feed data for demoing purposes

--- PrototypeJorge/PrototypeJorge/FeedTableViewController.swift
+++ PrototypeJorge/PrototypeJorge/FeedTableViewController.swift
@@ -5,18 +5,20 @@
 //  Created by jfuentescasillas on 11/09/23.
 //
 
+
 import UIKit
 
-class FeedTableViewController: UITableViewController {
 
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
+
+
+class FeedTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
