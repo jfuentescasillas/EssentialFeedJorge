fa1b8f6ab874844b9126c9272b19a0d30d8137b2
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-09-12 14:17:03 +0200

    Extract system under test (sut) creation to a factory method

--- EssentialFeedJorgeiOSTests/FeedViewControllerTests.swift
+++ EssentialFeedJorgeiOSTests/FeedViewControllerTests.swift
@@ -31,17 +31,15 @@ final class FeedViewController: UIViewController {
 
 final class FeedViewControllerTests: XCTestCase {
     func test_init_doesNotLoadFeed() {
-        let loader = LoaderSpy()
-        _ = FeedViewController(loader: loader)
+        let (_, loader) = makeSUT()
         
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
     
     func test_viewDidLoad_loadsFeed() {
-        let loader = LoaderSpy()
-        let sut = FeedViewController(loader: loader)
-        
+        let (sut, loader) = makeSUT()
+
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(loader.loadCallCount, 1)
@@ -49,8 +47,19 @@ final class FeedViewControllerTests: XCTestCase {
     
     
     // MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+       
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, loader)
+    }
+    
+    
     class LoaderSpy: FeedLoaderProtocol {
-         private(set) var loadCallCount: Int = 0
+        private(set) var loadCallCount: Int = 0
         
         
         func load(completion: @escaping (EssentialFeedJorge.LoadFeedResult) -> Void) {
