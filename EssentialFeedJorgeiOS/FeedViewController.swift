f0d567aedddb69dd7cc37e93637e274cf03a1e6c
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-09-14 13:32:53 +0200

    Commits for video: Effectively Test-driving MVC UI with Multiple Views -Retry failed image load on retry action

--- EssentialFeedJorgeiOS/FeedViewController.swift
+++ EssentialFeedJorgeiOS/FeedViewController.swift
@@ -78,15 +78,22 @@ final public class FeedViewController: UITableViewController {
         cell.feedImageRetryButton.isHidden = true
         cell.feedImageContainer.startShimmering()
         
-        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
-            guard let cell else { return }
+        let loadImage = { [weak self, weak cell] in
+            guard let self else { return }
             
-            let data = try? result.get()
-            let image = data.map(UIImage.init) ?? nil
-            cell.feedImageView.image = image
-            cell.feedImageRetryButton.isHidden = (image != nil)
-            cell.feedImageContainer.stopShimmering()
+            self.tasks[indexPath] = self.imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+                guard let cell else { return }
+                
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell.feedImageView.image = image
+                cell.feedImageRetryButton.isHidden = (image != nil)
+                cell.feedImageContainer.stopShimmering()
+            }
         }
+        
+        cell.onRetry = loadImage
+        loadImage()
         
         return cell
     }
--- EssentialFeedJorgeiOSTests/FeedViewControllerTests.swift
+++ EssentialFeedJorgeiOSTests/FeedViewControllerTests.swift
@@ -197,6 +197,30 @@ final class FeedViewControllerTests: XCTestCase {
     }
     
     
+    func test_feedImageViewRetryAction_retriesImageLoad() {
+        let image0 = makeImage(url: URL(string: "http://url-0.com")!)
+        let image1 = makeImage(url: URL(string: "http://url-1.com")!)
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [image0, image1])
+        
+        let view0 = sut.simulateFeedImageViewVisible(at: 0)
+        let view1 = sut.simulateFeedImageViewVisible(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url], "Expected two image URL request for the two visible views")
+        
+        loader.completeImageLoadingWithError(at: 0)
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url], "Expected only two image URL requests before retry action")
+        
+        view0?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url, image0.url], "Expected third imageURL request after first view retry action")
+        
+        view1?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url, image0.url, image1.url], "Expected fourth imageURL request after second view retry action")
+    }
+    
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
@@ -375,6 +399,11 @@ private extension FeedImageTableViewCell {
     var isShowingRetryAction: Bool {
         return !feedImageRetryButton.isHidden
     }
+    
+    
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
 }
 
 
@@ -405,3 +434,17 @@ private extension UIImage {
         return imgRenderer
     }
 }
+
+
+// MARK: - Extension. UIButton
+private extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
+
+
