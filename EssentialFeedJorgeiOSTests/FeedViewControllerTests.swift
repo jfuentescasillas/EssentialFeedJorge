452824f85ae4a8ce2be0862208d4d74f20aa8ad9
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-09-15 10:21:47 +0200

    Start commits for video:Refactoring Massive View Controllers Into Multiple Tiny MVCs -Extract UIImage test helper to a separate file

--- EssentialFeedJorgeiOSTests/FeedViewControllerTests.swift
+++ EssentialFeedJorgeiOSTests/FeedViewControllerTests.swift
@@ -470,23 +470,6 @@ private extension UIRefreshControl {
 }
 
 
-// MARK: - Extension. UIImage
-private extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        let format = UIGraphicsImageRendererFormat()
-        format.scale = 1
-        
-        let imgRenderer = UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
-            color.setFill()
-            rendererContext.fill(rect)
-        }
-        
-        return imgRenderer
-    }
-}
-
-
 // MARK: - Extension. UIButton
 private extension UIButton {
     func simulateTap() {
