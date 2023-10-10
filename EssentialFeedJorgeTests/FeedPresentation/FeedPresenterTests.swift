04f1df4fc3a4df1ed6ebb2b19aa489b84b612042
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-10-10 18:48:54 +0200

    "FeedPResenter" displays no error on "didStartLoadingFeed"

--- EssentialFeedJorgeTests/FeedPresentation/FeedPresenterTests.swift
+++ EssentialFeedJorgeTests/FeedPresentation/FeedPresenterTests.swift
@@ -9,39 +9,56 @@
 import XCTest
 
 
+struct FeedErrorViewModel {
+    let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+}
+
+
+protocol FeedErrorViewProtocol {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+
 final class FeedPresenter {
-    init(view: Any) {
-        
+    private let errorView: FeedErrorViewProtocol
+    
+    init(errorView: FeedErrorViewProtocol) {
+        self.errorView = errorView
     }
     
     
     func didStartLoadingFeed() {
-        
+        errorView.display(.noError)
     }
 }
 
 
 class FeedPresenterTests: XCTestCase {
     func test_init_doesNotSendMessagesToView() {
-        let (sut, view) = makeSUT()
-        
-        sut.didStartLoadingFeed()
-        
+        let (_, view) = makeSUT()
+       
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
     
     func test_didStartLoadingFeed_displaysNoErrorMessage() {
-//        let (_, view) = makeSUT()
+        let (sut, view) = makeSUT()
         
-//        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
     }
     
     
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(errorView: view)
+        
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         
@@ -49,11 +66,17 @@ class FeedPresenterTests: XCTestCase {
     }
     
     
-    private class ViewSpy {
+    private class ViewSpy: FeedErrorViewProtocol {
         enum Message: Equatable {
             case display(errorMessage: String?)
         }
         
-        let messages = [Message]()
+        
+        private(set) var messages = [Message]()
+        
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
     }
 }
