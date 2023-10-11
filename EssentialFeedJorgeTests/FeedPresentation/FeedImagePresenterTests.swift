7d331e8c4864efd997e1986a590d2bdbc9c63c0c
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-10-11 16:17:24 +0200

    Inline Data creation

--- EssentialFeedJorgeTests/FeedPresentation/FeedImagePresenterTests.swift
+++ EssentialFeedJorgeTests/FeedPresentation/FeedImagePresenterTests.swift
@@ -106,9 +106,8 @@ class FeedImagePresenterTests: XCTestCase {
     func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
         let (sut, view) = makeSUT(imageTransformer: fail)
         let image = uniqueImage()
-        let data = Data()
         
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
         
         let message = view.messages.first
         
@@ -123,11 +122,10 @@ class FeedImagePresenterTests: XCTestCase {
     
     func test_didFinishLoadingImageData_displaysImageOnSuccessfulTransformation() {
         let image = uniqueImage()
-        let data = Data()
         let transformedData = AnyImage()
         let (sut, view) = makeSUT(imageTransformer: { _ in transformedData })
         
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
         
         let message = view.messages.first
         
