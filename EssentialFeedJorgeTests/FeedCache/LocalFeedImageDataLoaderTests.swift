0323f29ae98940bd7443c7eba5a8188b0a5ed23b
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-11-14 17:13:03 +0100

    LocalFeedImageDataLoader.loadImageData requests stored data for URL from `FeedImageDataStoreProtocol`

--- EssentialFeedJorgeTests/FeedCache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeedJorgeTests/FeedCache/LocalFeedImageDataLoaderTests.swift
@@ -10,9 +10,31 @@ import XCTest
 import EssentialFeedJorge
 
 
-final class LocalFeedImageDataLoader {
-    init(store: Any) {
+// MARK: - FeedImageDataStoreProtocol
+protocol FeedImageDataStoreProtocol {
+    func retrieve(dataForURL url: URL)
+}
+
+
+// MARK: - Class LocalFeedImageDataLoader
+final class LocalFeedImageDataLoader: FeedImageDataLoaderProtocol {
+    private struct Task: FeedImageDataLoaderTask {
+        func cancel() {}
+    }
+    
+    
+    private let store: FeedImageDataStoreProtocol
+    
+    
+    init(store: FeedImageDataStoreProtocol) {
+        self.store = store
+    }
+    
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoaderProtocol.Result) -> Void) -> FeedImageDataLoaderTask {
+        store.retrieve(dataForURL: url)
         
+        return Task()
     }
 }
 
@@ -26,11 +48,21 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
     }
     
     
+    func test_loadImageDataFromURL_requestsStoredDataForURL() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+        
+        _ = sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve(dataFor: url)])
+    }
+
+    
     // MARK: - Helpers
-    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
-        let store = FeedStoreSpy()
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
+        let store = StoreSpy()
         let sut = LocalFeedImageDataLoader(store: store)
-      
+        
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         
@@ -38,7 +70,17 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
     }
     
     
-    private class FeedStoreSpy {
-        let receivedMessages = [Any]()
+    private class StoreSpy: FeedImageDataStoreProtocol {
+        enum Message: Equatable {
+            case retrieve(dataFor: URL)
+        }
+        
+        
+        private(set) var receivedMessages = [Message]()
+        
+        
+        func retrieve(dataForURL url: URL) {
+            receivedMessages.append(.retrieve(dataFor: url))
+        }
     }
 }
