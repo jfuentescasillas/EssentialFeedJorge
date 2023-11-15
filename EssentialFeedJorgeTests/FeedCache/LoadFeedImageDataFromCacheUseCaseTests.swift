cf60215f10b994bf8f4144ad94c393028b5a8bd4
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-11-15 11:32:39 +0100

    Extract cache feed image data use case into the new `CacheFeedImageDataUseCaseTests` test case class

--- EssentialFeedJorgeTests/FeedCache/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ EssentialFeedJorgeTests/FeedCache/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -19,17 +19,6 @@ class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
     }
     
     
-    func test_saveImageDataForURL_requestsImageDataInsertionForURL() {
-        let (sut, store) = makeSUT()
-        let url = anyURL()
-        let data = anyData()
-        
-        sut.save(data, for: url) { _ in }
-        
-        XCTAssertEqual(store.receivedMessages, [.insert(data: data, for: url)])
-    }
-    
-    
     func test_loadImageDataFromURL_requestsStoredDataForURL() {
         let (sut, store) = makeSUT()
         let url = anyURL()
