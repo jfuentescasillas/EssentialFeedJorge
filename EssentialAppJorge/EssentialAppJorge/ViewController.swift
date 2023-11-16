fcefd6edc42c36020ad63476015d4369f17db3b5
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-11-16 11:26:25 +0100

    Add EssentialAppJorge project

---
+++ EssentialAppJorge/EssentialAppJorge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialAppJorge
+//
+//  Created by jfuentescasillas on 16/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
