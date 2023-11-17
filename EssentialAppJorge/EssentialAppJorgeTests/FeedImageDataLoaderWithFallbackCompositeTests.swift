a70b1449b7a1b8475ee34963b8cbfccb1f86d290
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-11-17 17:17:41 +0100

    Move FeedImageDataLoaderWithFallbackComposite to production

--- EssentialAppJorge/EssentialAppJorgeTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialAppJorge/EssentialAppJorgeTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -8,47 +8,7 @@
 
 import XCTest
 import EssentialFeedJorge
-
-
-// MARK: - Class FeedImageDataLoaderWithFallbackComposite
-class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoaderProtocol {
-    private let primary: FeedImageDataLoaderProtocol
-    private let fallback: FeedImageDataLoaderProtocol
-    
-    
-    init(primary: FeedImageDataLoaderProtocol, fallback: FeedImageDataLoaderProtocol) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-    
-    
-    private class TaskWrapper: FeedImageDataLoaderTask {
-        var wrapped: FeedImageDataLoaderTask?
-        
-        
-        func cancel() {
-            wrapped?.cancel()
-        }
-    }
-    
-    
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoaderProtocol.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = TaskWrapper()
-        task.wrapped = primary.loadImageData(from: url) { [weak self] result in
-            guard let self else { return }
-            
-            switch result {
-            case .success:
-                completion(result)
-                
-            case .failure:
-                task.wrapped = self.fallback.loadImageData(from: url, completion: completion)
-            }
-        }
-        
-        return task
-    }
-}
+import EssentialAppJorge
 
 
 // MARK: - Class FeedImageDataLoaderWithFallbackCompositeTests
