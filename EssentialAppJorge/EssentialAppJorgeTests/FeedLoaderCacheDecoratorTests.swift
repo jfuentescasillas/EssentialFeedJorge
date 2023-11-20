e6267a5ef74de48f48f11744444dd4f29b047788
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-11-20 16:29:48 +0100

    FeedLoaderCacheDecorator.load delivers decoratee loader result (either success or failure)

---
+++ EssentialAppJorge/EssentialAppJorgeTests/FeedLoaderCacheDecoratorTests.swift
@@ -0,0 +1,89 @@
+//
+//  FeedLoaderCacheDecoratorTests.swift
+//  EssentialAppJorgeTests
+//
+//  Created by jfuentescasillas on 20/11/23.
+//
+
+
+import XCTest
+import EssentialFeedJorge
+
+
+// MARK: - Class FeedLoaderCacheDecorator
+final class FeedLoaderCacheDecorator: FeedLoaderProtocol {
+    private let decoratee: FeedLoaderProtocol
+    
+    
+    init(decoratee: FeedLoaderProtocol) {
+        self.decoratee = decoratee
+    }
+    
+    
+    func load(completion: @escaping (FeedLoaderProtocol.Result) -> Void) {
+        decoratee.load(completion: completion)
+    }
+}
+
+
+// MARK: - Class FeedLoaderCacheDecoratorTests
+final class FeedLoaderCacheDecoratorTests: XCTestCase {
+    func test_load_deliversFeedOnLoaderSuccess() {
+        let feed = uniqueFeed()
+        let loader = LoaderStub(result: .success(feed))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        
+        expect(sut, toCompleteWith: .success(feed))
+    }
+    
+    
+    func test_load_deliversErrorOnLoaderFailure() {
+        let loader = LoaderStub(result: .failure(anyNSError()))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        
+        expect(sut, toCompleteWith: .failure(anyNSError()))
+    }
+    
+    
+    // MARK: - Helpers
+    private func expect(_ sut: FeedLoaderProtocol, toCompleteWith expectedResult: FeedLoaderProtocol.Result, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedRsult in
+            switch(receivedRsult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+                
+            case (.failure, .failure):
+                break
+                
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedRsult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    
+    private func uniqueFeed() -> [FeedImage] {
+        return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
+    }
+    
+    
+    private class LoaderStub: FeedLoaderProtocol {
+        private let result: FeedLoaderProtocol.Result
+        
+        
+        init(result: FeedLoaderProtocol.Result) {
+            self.result = result
+        }
+        
+        
+        func load(completion: @escaping (FeedLoaderProtocol.Result) -> Void) {
+            completion(result)
+        }
+    }
+}
