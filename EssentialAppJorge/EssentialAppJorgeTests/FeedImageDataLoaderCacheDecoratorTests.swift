d7864eafd9fbe17ed9cdf1032f9909e87fc1d500
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-11-21 11:49:58 +0100

    test_loadImageData_deliversDataOnLoaderSuccess() running on green

--- EssentialAppJorge/EssentialAppJorgeTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialAppJorge/EssentialAppJorgeTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -56,6 +56,16 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
     }
     
     
+    func test_loadImageData_deliversDataOnLoaderSuccess() {
+        let imageData = anyData()
+        let (sut, loader) = makeSUT()
+        
+        expect(sut, toCompleteWith: .success(imageData), when: {
+            loader.complete(with: imageData)
+        })
+    }
+    
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImageDataLoaderProtocol, loader: LoaderSpy) {
         let loader = LoaderSpy()
@@ -65,6 +75,30 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         
         return (sut, loader)
+    }
+    
+    
+    private func expect(_ sut: FeedImageDataLoaderProtocol, toCompleteWith expectedResult: FeedImageDataLoaderProtocol.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+                
+            case (.failure, .failure):
+                break
+                
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead.", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1)
     }
     
     
@@ -94,6 +128,11 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
             }
             
             return task
+        }
+        
+        
+        func complete(with data: Data, at index: Int = 0) {
+            messages[index].completion(.success(data))
         }
     }
 }
