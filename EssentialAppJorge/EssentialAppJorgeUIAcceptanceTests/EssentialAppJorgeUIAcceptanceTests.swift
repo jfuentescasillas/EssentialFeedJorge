e51af6dae1a996822cb9746a8d48aedcc8e5bd81
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-11-23 13:03:39 +0100

    Displays empty feed when customer has no connectivity and no cache

--- EssentialAppJorge/EssentialAppJorgeUIAcceptanceTests/EssentialAppJorgeUIAcceptanceTests.swift
+++ EssentialAppJorge/EssentialAppJorgeUIAcceptanceTests/EssentialAppJorgeUIAcceptanceTests.swift
@@ -35,4 +35,14 @@ final class EssentialAppJorgeUIAcceptanceTests: XCTestCase {
         let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstCachedImage.exists)
     }
+    
+    
+    func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivity() {
+        let app = XCUIApplication()
+        app.launchArguments = ["-reset", "-connectivity", "offline"]
+        app.launch()
+        
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 0)
+    }
 }
