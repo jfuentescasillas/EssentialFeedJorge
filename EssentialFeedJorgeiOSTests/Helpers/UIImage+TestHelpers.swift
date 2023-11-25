e4476edf4555657470238ca93965934e09f23d61
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-11-25 12:41:36 +0100

    Record feed with content snapshot

---
+++ EssentialFeedJorgeiOSTests/Helpers/UIImage+TestHelpers.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+TestHelpers.swift
+//  EssentialFeedJorgeiOSTests
+//
+//  Created by jfuentescasillas on 25/11/23.
+//
+
+
+import UIKit
+
+
+// MARK: - Extension. UIImage
+public extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        
+        let imgRenderer = UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(rect)
+        }
+        
+        return imgRenderer
+    }
+}
