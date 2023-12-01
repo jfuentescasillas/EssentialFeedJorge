eb24ef7f05dc588f595b4384562d12a8c10a21dc
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-12-01 11:24:12 +0100

    Duplicate RemoteFeedLoader as RemoteLoader

---
+++ EssentialFeedJorge/SharedAPI/RemoteLoader.swift
@@ -0,0 +1,55 @@
+//
+//  RemoteLoader.swift
+//  EssentialFeedJorge
+//
+//  Created by jfuentescasillas on 01/12/23.
+//
+
+
+import Foundation
+
+
+public final class RemoteLoader: FeedLoaderProtocol {
+    private let url: URL
+    private let client: HTTPClientProtocol
+
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
+    
+    public typealias Result = FeedLoaderProtocol.Result
+
+    
+    public init(url: URL, client: HTTPClientProtocol) {
+        self.url = url
+        self.client = client
+    }
+
+    
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+
+            switch result {
+            case let .success((data, response)):
+                completion(RemoteLoader.map(data, from: response))
+
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+    
+
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, from: response)
+            return .success(items)
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+}
