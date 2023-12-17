1cffc70f4c2a2a33fa871b4c600da9bff1259702
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-12-17 11:18:33 -0600

    Extract shared ListViewController snapshot tests

--- EssentialFeedJorgeiOSTests/FeedSnapshotTests.swift
+++ EssentialFeedJorgeiOSTests/FeedSnapshotTests.swift
@@ -12,30 +12,12 @@ import EssentialFeedJorgeiOS
 
 
 class FeedSnapshotTests: XCTestCase {
-    func test_emptyFeed() {
-        let sut = makeSUT()
-        sut.display(emptyFeed())
-        
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
-    }
-    
-    
     func test_feedWithContent() {
         let sut = makeSUT()
         sut.display(feedWithContent())
         
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_CONTENT_light")
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_CONTENT_dark")
-    }
-    
-    
-    func test_feedWithErrorMessage() {
-        let sut = makeSUT()
-        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
-        
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
     }
     
     
