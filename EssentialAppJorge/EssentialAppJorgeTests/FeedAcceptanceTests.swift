f84cefc5500878a6fcbea1468da686a31f2fddb8
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-02-10 17:43:54 +0100

    Show comments on image selection

--- EssentialAppJorge/EssentialAppJorgeTests/FeedAcceptanceTests.swift
+++ EssentialAppJorge/EssentialAppJorgeTests/FeedAcceptanceTests.swift
@@ -39,7 +39,7 @@ class FeedAcceptanceTests: XCTestCase {
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
         let feed = launch(httpClient: .offline, store: .empty)
         
-        XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 0)        
+        XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 0)
     }
     
     
@@ -61,6 +61,14 @@ class FeedAcceptanceTests: XCTestCase {
     }
     
     
+    func test_onFeedImageSelection_displaysComments() {
+        let comments = showCommentsForFirstImage()
+        
+        XCTAssertEqual(comments.numberOfRenderedComments(), 1)
+        XCTAssertEqual(comments.commentMessage(at: 0), makeCommentMessage())
+    }
+    
+    
     // MARK: - Helpers
     private func launch(httpClient: HTTPClientStub = .offline, store: InMemoryFeedStore = .empty) -> ListViewController {
         let sut = SceneDelegate(httpClient: httpClient, store: store)
@@ -79,6 +87,18 @@ class FeedAcceptanceTests: XCTestCase {
     }
     
     
+    private func showCommentsForFirstImage() -> ListViewController {
+        let feed = launch(httpClient: .online(response), store: .empty)
+        feed.simulateTapOnFeedImage(at: 0)
+        
+        RunLoop.current.run(until: Date())
+        
+        let nav = feed.navigationController
+        
+        return nav?.topViewController as! ListViewController
+    }
+    
+    
     private func response(for url: URL) -> (Data, HTTPURLResponse) {
         let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
         
@@ -90,10 +110,13 @@ class FeedAcceptanceTests: XCTestCase {
         switch url.path {
         case "/image-1", "/image-2":
             return makeImageData()
-
+            
         case "/essential-feed/v1/feed":
-                    return makeFeedData()
-
+            return makeFeedData()
+            
+        case "/essential-feed/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments":
+            return makeCommentsData()
+            
         default:
             return Data()
         }
@@ -110,5 +133,24 @@ class FeedAcceptanceTests: XCTestCase {
             ["id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086", "image": "http://feed.com/image-1"],
             ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-2"]
         ]])
+    }
+    
+    
+    private func makeCommentsData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            [
+                "id": UUID().uuidString,
+                "message": makeCommentMessage(),
+                "created_at": "2020-05-20T11:24:59+0000",
+                "author": [
+                    "username": "a username"
+                ]
+            ],
+        ]])
+    }
+    
+    
+    private func makeCommentMessage() -> String {
+        "a message"
     }
 }
