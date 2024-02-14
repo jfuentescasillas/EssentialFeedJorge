5fb273c3dc64a2e8a88a18dbf682670bc0bc7db9
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-02-14 17:44:33 +0100

    Remove commented code (update is done, so no need to keep it)

--- EssentialFeedJorgeiOS/FeedUI/Controllers/LoadMoreCellController.swift
+++ EssentialFeedJorgeiOS/FeedUI/Controllers/LoadMoreCellController.swift
@@ -35,6 +35,7 @@ extension LoadMoreCellController: UITableViewDataSource {
         return 1
     }
 
+
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         cell.selectionStyle = .none
                 
@@ -82,57 +83,3 @@ extension LoadMoreCellController: ResourceErrorViewProtocol {
         cell.message = viewModel.message
     }
 }
-
-
-/*  ACTUALIZACIÓN PENDIENTE
- 
-public class LoadMoreCellController: NSObject, UITableViewDataSource, UITableViewDelegate {
-   private let cell = LoadMoreCell()
-    private let callback: () -> Void
-    private var offsetObserver: NSKeyValueObservation?
-    
-    
-    public init(callback: @escaping () -> Void) {
-        self.callback = callback
-    }
-    
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        cell.selectionStyle = .none
-        
-        return cell
-    }
-    
-    
-    public func tableView(_ tableView: UITableView, willDisplay: UITableViewCell, forRowAt indexPath: IndexPath) {
-        reloadIfNeeded()
-        
-        offsetObserver = tableView.observe(\.contentOffset, options: .new) { [weak self] (tableView, _) in
-            guard tableView.isDragging else { return }
-            
-            self?.reloadIfNeeded()
-        }
-    }
-    
-    
-    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        offsetObserver = nil
-    }
-    
-    
-    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        reloadIfNeeded()
-    }
-    
-    
-    private func reloadIfNeeded() {
-        guard !cell.isLoading else { return }
-        
-        callback()
-    }
-} */
